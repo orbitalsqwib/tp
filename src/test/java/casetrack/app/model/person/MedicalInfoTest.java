7e6eab9aa88f40b91f1104a71cc25debe5a8a991
user <177832011+niyniy123@users.noreply.github.com>
2025-10-10 00:28:28 +0800

    Reorder imports in MedicalInfoTest.java

--- src/test/java/casetrack/app/model/person/MedicalInfoTest.java
+++ src/test/java/casetrack/app/model/person/MedicalInfoTest.java
@@ -1,10 +1,10 @@
 package casetrack.app.model.person;
 
 import static casetrack.app.testutil.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
