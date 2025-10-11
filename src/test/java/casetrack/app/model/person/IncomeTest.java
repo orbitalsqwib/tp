4b3d23b4cdeb5554742fb70d73bb040667edac22
user <177832011+niyniy123@users.noreply.github.com>
2025-10-11 18:44:24 +0800

    Fix BigDecimal import codestyle violation

--- src/test/java/casetrack/app/model/person/IncomeTest.java
+++ src/test/java/casetrack/app/model/person/IncomeTest.java
@@ -6,8 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
 
 public class IncomeTest {
 
