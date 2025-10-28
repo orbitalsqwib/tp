34a306d0054beb4b479d900928a8ac63fd5140a7
Jeya <e1356515@u.nus.edu>
2025-10-28 09:59:37 +0800

    Fix supoort for all numeric names

--- src/test/java/casetrack/app/model/person/NameTest.java
+++ src/test/java/casetrack/app/model/person/NameTest.java
@@ -34,11 +34,13 @@ public class NameTest {
 
         // valid name
         assertTrue(Name.isValidName("peter jack")); // alphabets only
-        assertTrue(Name.isValidName("12345")); // numbers only
         assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric characters
         assertTrue(Name.isValidName("Capital Tan")); // with capital letters
         assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
         assertTrue(Name.isValidName("Ravichandran S/O Tharumalinga")); // indian names
+
+        assertFalse(Name.isValidName("12345")); // numbers only
+
     }
 
     @Test
