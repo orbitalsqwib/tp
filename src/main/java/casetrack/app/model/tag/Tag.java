d1be9d50786696006027f01d76f83493321e234c
Jeya <e1356515@u.nus.edu>
2025-10-28 12:14:53 +0800

    Update tag to allow hypen

--- src/main/java/casetrack/app/model/tag/Tag.java
+++ src/main/java/casetrack/app/model/tag/Tag.java
@@ -9,8 +9,8 @@ import static java.util.Objects.requireNonNull;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
-    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric and may contain hyphens";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}-]+";
 
     public final String tagName;
 
