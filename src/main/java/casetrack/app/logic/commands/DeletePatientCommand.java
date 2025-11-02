946483e9c81d46b84bdfe58d0f31b776262824b5
Eugene L <orbital@sqwib.works>
2025-11-02 21:56:08 +0800

    Add underscore

--- src/main/java/casetrack/app/logic/commands/DeletePatientCommand.java
+++ src/main/java/casetrack/app/logic/commands/DeletePatientCommand.java
@@ -16,7 +16,7 @@ public class DeletePatientCommand extends DeleteCommand {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the patient identified by the index number used in the displayed patient list.\n"
-            + "Parameters: <PATIENT INDEX> (must be a positive integer)\n"
+            + "Parameters: <PATIENT_INDEX> (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Patient: %1$s";
